73760fda06bd5b1e90c9918e288ba55ee6f6eb10
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-02-21 18:17:03 +0100

    add: picocss, possible to create, delete student and destroy all students

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,128 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import style from "./assets/css/app.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const getStudents = localStorage.getItem("students");
+
+  const [teams, setTeams] = useState([]);
+  const [students, setStudents] = useState(
+    getStudents ? getStudents.split(",") : [],
+  );
+  const nbTeams = useRef(4);
+  const name = useRef("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(nbTeams.current.value);
+  };
+
+  const handleReset = (event) => {
+    event.preventDefault();
+    localStorage.removeItem("students");
+    setStudents([]);
+  };
+
+  const handleAdd = (event) => {
+    event.preventDefault();
+    localStorage.setItem("students", [...students, name.current.value]);
+    setStudents([...students, name.current.value]);
+    name.current.value = "";
+  };
+
+  const handleDelete = (event) => {
+    event.preventDefault();
+    const index = event.target.parentNode.dataset.index;
+    const newStudents = students.filter((student, i) => i !== +index);
+    localStorage.setItem("students", newStudents);
+    setStudents(newStudents);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className="container-fluid">
+      <h1>Générateur de groupe</h1>
+      <section className={style.section}>
+        <div>
+          <h2>Équipes</h2>
+          <p>
+            Il vous suffit de saisir le nombre d'équipes souhaitées et de
+            cliquer sur le bouton ci-dessous.
+          </p>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <fieldset className="grid">
+                <label htmlFor="nbTeams">
+                  Nombre d'équipes
+                  <input
+                    type="number"
+                    id="nbTeams"
+                    className="form-control"
+                    min="1"
+                    max="10"
+                    ref={nbTeams}
+                    defaultValue={nbTeams.current}
+                    required
+                  />
+                </label>
+                <button type="submit" className="outline">
+                  Générer des équipes
+                </button>
+              </fieldset>
+            </div>
+          </form>
+        </div>
+        <div>
+          <h2>Listes des élèves</h2>
+          <p>
+            Vous pouvez saisir le nom des élèves, un par ligne, et cliquer sur
+            le bouton ci-dessous pour les ajouter à la liste.
+          </p>
+          <ul className={style.grid_students}>
+            {students.map((student, index) => (
+              <li key={index} className={style.li} data-index={index}>
+                {student}
+                <button className="outline" onClick={handleDelete}>
+                  ❌
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <form onSubmit={handleAdd}>
+            <input type="text" placeholder="Bob l'éponge" ref={name} />
+            <fieldset className="grid">
+              <button type="submit" className="outline">
+                Ajouter un élève
+              </button>
+              <button
+                type="reset"
+                className="outline contrast"
+                onClick={handleReset}
+              >
+                Vider la liste
+              </button>
+            </fieldset>
+          </form>
+          <hr />
+          <details>
+            <summary>ℹ️ Syntaxe</summary>
+            <p>
+              Le fichier CSV doit contenir une colonne <code>name</code> avec le
+              nom des personnes de la team
+            </p>
+          </details>
+          <hr />
+          <form>
+            <label htmlFor="file">Importer un fichier csv</label>
+            <input type="file" accept="application/csvm+json" />
+            <button type="submit">Envoyer</button>
+          </form>
+        </div>
+      </section>
+      <section>
+        <h2>Tirage au sort</h2>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
