f014f4d8d007c55678063f483a4ff313f35a5841
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-03-22 11:20:10 +0100

    update projet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import { PeopleTag, Heart } from "iconoir-react";
 function Footer() {
-  return (
-    <footer style={{ textAlign: "center", marginTop: "2rem" }}>
-      <p>
-        Ce petit projet vous est proposé par{" "}
-        <a href="https://github.com/GorskiAnthony">Moi-même</a> <PeopleTag />
-      </p>
-        <p>Merci à <a href="https://ioayoub.fr/">Ayoub</a> pour l'idée <Heart /> sur toi </p>
-    </footer>
-  );
+	return (
+		<footer style={{ textAlign: "center", marginTop: "2rem" }}>
+			<p>
+				Ce petit projet vous est proposé par{" "}
+				<a href="https://github.com/GorskiAnthony">Moi-même</a>{" "}
+				<PeopleTag />
+			</p>
+			<p>
+				Merci à <a href="https://ioayoub.fr/">Ayoub</a> pour l'idée{" "}
+				<Heart /> sur toi{" "}
+			</p>
+		</footer>
+	);
 }
 
 export default Footer;
--- src/services/handlers.js
+++ src/services/handlers.js
@@ -3,15 +3,42 @@ import { toastError, toastNeutral, toastSuccess } from "./toast.js";
 import jsConfetti from "./confetti.js";
 
 /**
- * Description: Crée des équipes avec des étudiants en générant trois tentatives de groupes différents
+ * Mélange les éléments d'un tableau
+ * @param {Array} array - Le tableau à mélanger
+ * @returns {Array} - Le tableau mélangé
+ */
+function shuffleArray(array) {
+	let currentIndex = array.length,
+		randomIndex;
+
+	// Tant qu'il reste des éléments à mélanger...
+	while (currentIndex != 0) {
+		// Prendre un élément restant...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// Et l'échanger avec l'élément actuel.
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
+
+	return array;
+}
+
+/**
+ * Crée des équipes avec des étudiants en générant trois tentatives de groupes différents,
+ * en s'assurant que les étudiants ne se retrouvent pas dans les mêmes groupes.
  * @param {Object} event - Objet d'événement
  * @param {Number} groups - Nombre de groupes
  * @param {Function} setter - Fonction pour mettre à jour l'état
- * @returns {Array} - Tableau de trois ensembles d'équipes différents
  */
 const handleCreateTeams = (event, groups, setter) => {
 	event.preventDefault();
 	const originalStudents = getItems("students")?.split(",");
+	const threshold = Math.floor(originalStudents.length / groups) - 1;
+
 	if (originalStudents === undefined)
 		return toastError("Herm.. Il n'y a pas d'élèves");
 	if (originalStudents.length < groups)
@@ -19,45 +46,79 @@ const handleCreateTeams = (event, groups, setter) => {
 			"Herm.. Il n'y a pas assez d'élèves pour créer des équipes"
 		);
 
+	let previousTeams = [];
 	const allCyclesTeams = [];
+	let attempts = 0; // Pour éviter une boucle infinie
 
 	for (let cycle = 0; cycle < 3; cycle++) {
-		let teams;
-		do {
-			const students = [...originalStudents];
-			teams = [];
+		// Mettre une limite aux tentatives
+		if (attempts > 10) {
+			toastError(
+				"Impossible de créer des équipes uniques après plusieurs tentatives."
+			);
+			break;
+		}
 
-			for (let i = 0; i < groups; i++) {
-				teams.push([]);
-			}
+		let teams = [];
+		let students = shuffleArray([...originalStudents]);
 
-			while (students.length > 0) {
-				for (let i = 0; i < groups; i++) {
-					if (students.length === 0) break;
-					const randomIndex = Math.floor(
-						Math.random() * students.length
-					);
-					const student = students.splice(randomIndex, 1)[0];
-					teams[i].push(student);
-				}
-			}
-		} while (
-			allCyclesTeams.some((existingTeams) =>
-				areTeamsSimilar(existingTeams, teams)
-			)
-		);
+		for (let i = 0; i < groups; i++) {
+			teams.push([]);
+		}
 
-		allCyclesTeams.push(teams);
-	}
-	setter(allCyclesTeams);
-};
+		students.forEach((student, index) => {
+			teams[index % groups].push(student);
+		});
 
-function areTeamsSimilar(teamsA, teamsB) {
-	for (let i = 0; i < teamsA.length; i++) {
-		if (teamsA[i].sort().join() !== teamsB[i].sort().join()) {
-			return false;
+		if (areTeamsUnique(previousTeams, teams, threshold)) {
+			// Autoriser un coéquipier identique
+			allCyclesTeams.push(teams);
+			previousTeams.push(...teams.map((team) => [...team])); // Copier les équipes pour référence future
+		} else {
+			// Réessayer si les équipes sont trop similaires
+			cycle--;
+			attempts++;
 		}
 	}
+
+	if (allCyclesTeams.length === 3) {
+		// Vérifier si 3 ensembles d'équipes ont été créés avec succès
+		setter(allCyclesTeams);
+	}
+};
+
+/**
+ * Vérifie si les nouvelles équipes sont uniques par rapport aux équipes précédentes
+ * @param {Array} previousTeams - Les équipes des cycles précédents
+ * @param {Array} newTeams - Les nouvelles équipes à vérifier
+ * @returns {Boolean} - Vrai si les équipes sont uniques, faux autrement
+ */
+function areTeamsUnique(previousTeams, newTeams, threshold) {
+	// S'il n'y a pas d'équipes précédentes, toutes les nouvelles équipes sont considérées comme uniques
+	if (previousTeams.length === 0) return true;
+
+	// Vérifiez l'unicité des équipes par rapport aux équipes précédentes
+	for (const newTeam of newTeams) {
+		let uniqueCount = 0;
+
+		// Vérifiez l'équipe actuelle par rapport à toutes les équipes précédentes
+		for (const prevTeam of previousTeams) {
+			const commonMembers = newTeam.filter((student) =>
+				prevTeam.includes(student)
+			).length;
+
+			// Si cette équipe a plus de [threshold] membres en commun avec une équipe précédente, augmentez le compteur
+			if (commonMembers > threshold) {
+				uniqueCount++;
+			}
+
+			// Si l'équipe actuelle n'est pas unique par rapport à suffisamment d'équipes précédentes, retournez faux
+			if (uniqueCount > previousTeams.length - threshold) {
+				return false;
+			}
+		}
+	}
+
 	return true;
 }
 
